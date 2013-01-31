1f65ba71d661cb0b690d2fd9709c3a21fec5eac1
Andrew Wedgbury <wedge@sconemad.com>
2013-01-31 21:59:26 +0000

    Various version number updates for consistency

--- sconex/sconex.cpp
+++ sconex/sconex.cpp
@@ -35,7 +35,7 @@ VersionTag& version()
 const std::string& sconex_copyright()
 {
   static std::string s_copyright = 
-    "Copyright (c) 2000-2011 Andrew Wedgbury <wedge@sconemad.com>";
+    "Copyright (c) 2000-2013 Andrew Wedgbury <wedge@sconemad.com>";
   return s_copyright;
 }
 
